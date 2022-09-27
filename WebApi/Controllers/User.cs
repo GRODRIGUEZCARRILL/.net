ad397666da8bee3567b0457e07034c09209f57b3
Guillermo Rodriguez <grodriguezcarrill@deloitte.com>
2022-09-27 18:03:20 -0500

    Guillermo

---
+++ WebApi/Controllers/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controller
+{
+    [HttpPost("Add")]
+    public User CreateUser(int id, string name, string email, int phone)
+    {
+        userId = id;
+        userName = name;
+        userEmail = email;
+        userPhone = phone;
+
+    };
+    return user;
+}
+
+[HttpGet("Id")]
---
+++ users.cs
@@ -0,0 +1,11 @@
+﻿namespace users.Models
+{
+    public class users
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string Email { get; set; }
+        public int ? phone { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
